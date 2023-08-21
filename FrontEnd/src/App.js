6b960532ccc2beeadd2bd37c9fa5f92a1222c528
Arvindh-1509 <rockstararvindh@gmail.com>
2023-08-21 20:00:46 +0530

    3rd

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <h3>Your Tasks</h3>
+      <div className='todos'>
+        <div className='todo'>
+          <div className='checkbox'>
+ 
+          </div>
+          <div className='text'>
+          <p>Get a pizza</p>
+          </div>
+          <div className='deletetodo'>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
